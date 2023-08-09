11a85744aa7f4b5277f67f710ad96d97237e3b72
sneusar <sneusar@eafit.edu.co>
2023-08-08 22:03:34 -0500

    Visualización de películas

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 
+from .models import Movie
+
 # Create your views here.
 
+
 def home(request):
-    return render(request, 'home.html', {'name':'Santiago Neusa'})
+
+    searchTerm = request.GET.get('searchMovie')
+
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+
 
 def about(request):
     return render(request, 'about.html')
+
